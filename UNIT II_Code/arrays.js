45432d6d408e179ca0791448cfd574837f5fc284
rodrigo-orozco <rodro14@DESKTOP-5H5Q4PI.localdomain>
2021-10-12 08:10:44 -0500

    Adding arrays topic done

---
+++ UNIT II_Code/arrays.js
@@ -0,0 +1,50 @@
+//Create an array
+
+var myArray = [1,2,3,4,5]; // Literal Array
+// myArray2 = new Array[1,2,3]; // Using constructor
+
+//Longitud de arrays
+
+const ln = myArray.length;
+console.log(ln);
+
+// Show Array;
+console.log(myArray);
+
+//Iterate arrays
+for (let i = 0; i < ln; i++) {
+    console.log(myArray[i]);
+}
+
+console.log("-----");
+
+for (let index in myArray) {
+    console.log(myArray[index]);
+}
+
+console.log("-----");
+
+for (let i of myArray) {
+    console.log(i);
+}
+
+// Adding elements
+
+let data = [];
+console.log(data);
+const temp = 27;
+data.push(temp);
+console.log(data);
+data.push(68);
+console.log(data);
+
+//Insert first element
+
+data.unshift(12);
+console.log(data);
+
+// Splice --> Insert multiple data
+
+data.splice(1,0,3,4); // splice(index, deletedata, add...)
+console.log(data);
+
