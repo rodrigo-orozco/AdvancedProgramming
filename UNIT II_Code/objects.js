cc7236317bbc919da7f92994e0bd90937feb19cd
rodrigo-orozco <rodro14@DESKTOP-5H5Q4PI.localdomain>
2021-10-14 07:38:00 -0500

    First Object

---
+++ UNIT II_Code/objects.js
@@ -0,0 +1,13 @@
+const BMO = {
+    name: "BMO",
+    type: "Video Game Console",
+    friends: ["Fin", "Jake", "Burbujita"],
+    dance: function(){
+        console.log("BMO is dancing...");
+        return;
+    },
+};
+
+for (key in BMO){
+    console.log(`${key}: ${BMO[key]}`);
+};
